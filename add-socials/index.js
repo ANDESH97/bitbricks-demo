eede0ce22322ad5b5dcde7057b57ff4fae7a6043
ankit-deshmukh <ankit.deshmukh@smallcase.com>
2022-09-04 14:43:39 +0530

    feat: added npm to project

---
+++ add-socials/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', function (req, res) {
+  console.log("home");
+  res.sendFile(path.join(__dirname + '/add-socials-popup.html'));
+});
+
+// app.get('/.well-known/apple-app-site-association', function (req, res) {
+
+//   console.log("sending apple app site association file ...");
+//   res.header('Content-Type', 'application/json');
+//   // res.setHeader('Content-Type', 'application/json');
+//   res.sendFile(path.join(__dirname + '/public/.well-known/apple-app-site-association'));
+// });
+
+app.listen(process.env.PORT || 7777);
