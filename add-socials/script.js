371b0018c27c2bdf9172e2f4f7efab72214c3058
ankit-deshmukh <ankit.deshmukh@smallcase.com>
2022-09-04 15:16:27 +0530

    Update script.js
    build: requesting header 'Origin'

--- add-socials/script.js
+++ add-socials/script.js
@@ -169,7 +169,8 @@ function previewImageAndUpload(event) {
               'Content-Type': 'application/json',
               'Accept':'*/*',
             //   'Access-Control-Allow-Origin': 'http://localhost:5500',
-            'Access-Control-Allow-Headers':'Origin',
+            // 'Access-Control-Allow-Headers':'Origin',
+            'Access-Control-Request-Headers':'Origin',
               'Access-Control-Allow-Origin': 'https://bitbricks-demo.herokuapp.com/',
               'Access-Control-Allow-Credentials': 'true'
             },
