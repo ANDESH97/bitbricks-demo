e07b78964753c05680a0a0352effe1cc3cda0dfe
ankit-deshmukh <ankit.deshmukh@smallcase.com>
2023-08-23 13:35:37 +0530

    build: checkin

--- add-socials/script.js
+++ add-socials/script.js
@@ -1,3 +1,27 @@
+//Fetch socials for id: 63287c8440de249e4c208e52
+//Space id: wHwQ1QLKh571DO4Cynake
+
+function loadSocials() {
+    
+    var config = {
+        method: 'get',
+        url: 'https://zswm2a102j.execute-api.us-east-1.amazonaws.com/dev/socials/63287c8440de249e4c208e52',
+        headers: { },
+        data : ''
+      };
+      
+    //   axios(config)
+    //   .then(function (response) {
+
+    //     if(response.data.data != null) {
+    //         console.log(JSON.stringify(response.data));
+    //     }
+    //   })
+    //   .catch(function (error) {
+    //     console.log(error);
+    //   });
+}
+
 function formFieldFocused(inputElement) {
     document.getElementById(inputElement.id).setAttribute('size', '60');
     const name = inputElement.name.substring(7);
@@ -53,7 +77,8 @@ async function fetchDefaultPlaceholderCard() {
 
     var li = document.createElement("li");
     li.classList.add("add-new-social-card");
-    li.id = 'new-social-card-template';
+    let x = Math.floor((Math.random() * 1000) + 1);
+    li.id = 'new-social-card-template-'.concat(x);
     li.innerHTML = myHtml;
 
     const socialListElement = document.getElementById("socials-list");
@@ -68,22 +93,77 @@ async function fetchDefaultPlaceholderCard() {
 
         document.getElementById("existing-socials-list").appendChild(ul);
     }
-
 }
 
-function submitSocialsDetail() {
-    let socialTextInput = document.getElementById("social_title");
-    let socialUrlInput = document.getElementById("social_url");
+async function submitSocialsDetail(element) {
+    var li = element.closest('li');
 
-    console.log(socialTextInput.value);
-    console.log(socialUrlInput.value);
+    var socialTitleInput = li.querySelector('input.social-title-input');
+    var socialUrlInput = li.querySelector('input.social-url-input');
+    var socialInputThumnail = li.querySelector('input.social-image-input');
 
-    var jsonObj = {
-        "socialTitle": socialTextInput.value,
-        "socialUrl": socialUrlInput.value
-    }
+    const file = socialInputThumnail.files[0];
+    const extension = file.name.substring(file.name.lastIndexOf('.') + 1);
+    const fileMb = file.size / 1024 ** 2;
+    
+    if(fileMb > 2) {
+        alert('Please select a file less than 2MB in size');
+        return;
+    }    
+    
+    let formData = new FormData();
+    formData.set('file', file);
 
-    console.log(jsonObj);
+    var data = JSON.stringify({
+        "body": await getBase64(file),
+        "extension": extension
+    })
+
+    var config = {
+        method: 'post',
+        url: 'https://zswm2a102j.execute-api.us-east-1.amazonaws.com/dev/socials/upload',
+        headers: { 
+          'Content-Type': 'application/json',
+        },
+        data : data
+      };
+
+      axios(config)
+      .then(function (response) {
+        // console.log(JSON.stringify(response.data));
+        var jsonObj = {
+            "socialKey": socialTitleInput.value,
+            "socialValue": socialUrlInput.value,
+            "socialThumbnailPath": response.data.data.key
+        }
+        // console.log(jsonObj);
+        createNewSocial(jsonObj);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+}
+
+function createNewSocial(newSocialJson) {
+
+    console.log(newSocialJson);
+
+    var config = {
+        method: 'post',
+        url: 'https://zswm2a102j.execute-api.us-east-1.amazonaws.com/dev/socials',
+        headers: { 
+          'Content-Type': 'application/json'
+        },
+        data : newSocialJson
+      };
+      
+      axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
 }
 
 function removeSocial() {
@@ -114,61 +194,59 @@ function selectImage() {
     imageUploader.click();
 }
 
-function previewImageAndUpload(event) {
+async function previewImageAndUpload(event) {
 
     if (event.target.files.length > 0) {
         var src = URL.createObjectURL(event.target.files[0]);
+        // const file = event.target.files[0];
+        // const extension = file.name.substring(file.name.lastIndexOf('.') + 1);
+        // const fileMb = file.size / 1024 ** 2;
         
-        const file = event.target.files[0];
+        // if(fileMb > 2) {
+        //     alert('Please select a file less than 2MB in size');
+        //     return;
+        // }
+        var li = event.target.closest('li');
         
-        const extension = file.name.substring(file.name.lastIndexOf('.') + 1);
-
-        // console.log("file extension: " + extension);
-
-        const fileMb = file.size / 1024 ** 2;
-        // console.log('File size: ' + fileMb);
-        
-        if(fileMb > 2) {
-            alert('Please select a file less than 2MB in size');
-            return;
-        }
-        
-        var preview = document.getElementById('thumbnail-preview');
+        // var preview = document.getElementById('thumbnail-preview');
+        var preview = li.querySelector('img.social-thumbnail-preview');
         preview.src = src;
         preview.style.display = "block";
 
-        // getBase64(file);
+        // // getBase64(file);
         
-        let formData = new FormData();
-        formData.set('file', file);
+        // let formData = new FormData();
+        // formData.set('file', file);
 
-        var data = JSON.stringify({
-            "body": getBase64(file),
-            "extension": extension
-        })
+        // var data = JSON.stringify({
+        //     "body": await getBase64(file),
+        //     "extension": extension
+        // })
 
-        var config = {
-            method: 'post',
-            url: 'https://zswm2a102j.execute-api.us-east-1.amazonaws.com/dev/socials/upload',
-            headers: { 
-              'Content-Type': 'application/json',
-            //   'Accept':'*/*',
-            //   'Access-Control-Allow-Origin': 'http://localhost:5500',
-            // 'Access-Control-Allow-Headers':'Origin',
-            // 'Access-Control-Request-Headers':'Origin',
-            //   'Access-Control-Allow-Origin': 'https://bitbricks-demo.herokuapp.com/',
-            //   'Access-Control-Allow-Credentials': 'true'
-            },
-            data : data
-          };
+        // console.log('data: \n' + data);
 
-          axios(config)
-          .then(function (response) {
-            console.log(JSON.stringify(response.data));
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
+        // var config = {
+        //     method: 'post',
+        //     url: 'https://zswm2a102j.execute-api.us-east-1.amazonaws.com/dev/socials/upload',
+        //     headers: { 
+        //       'Content-Type': 'application/json',
+        //     //   'Accept':'*/*',
+        //     //   'Access-Control-Allow-Origin': 'http://localhost:5500',
+        //     // 'Access-Control-Allow-Headers':'Origin',
+        //     // 'Access-Control-Request-Headers':'Origin',
+        //     //   'Access-Control-Allow-Origin': 'https://bitbricks-demo.herokuapp.com/',
+        //     //   'Access-Control-Allow-Credentials': 'true'
+        //     },
+        //     data : data
+        //   };
+
+        //   axios(config)
+        //   .then(function (response) {
+        //     console.log(JSON.stringify(response.data));
+        //   })
+        //   .catch(function (error) {
+        //     console.log(error);
+        //   });
         // axios.post("http://localhost:3001/upload-single-file", formData, {
         //     onUploadProgress: progressEvent => {
         //         const percentCompleted = Math.round(
@@ -184,17 +262,35 @@ function previewImageAndUpload(event) {
     }
 }
 
-function getBase64(file) {
-    var reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = function () {
-        var base64String = reader.result;
-        const base64Prefix = 'data:image/svg+xml;base64,'
-        if(base64String.startsWith(base64Prefix)) {
-            base64String = base64String.slice(base64Prefix.length);
-        }
-    };
-    reader.onerror = function (error) {
-      console.log('Error: ', error);
-    };
+async function getBase64(file) {
+    return new Promise( (resolve, reject) => {
+        var reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = async function () {
+            var base64String = reader.result;
+            const base64Prefix = 'data:image/png;base64,'
+            resolve(base64String.replace(base64Prefix,''));
+            // if(base64String.startsWith(base64Prefix)) {
+            //     // base64String = base64String.slice(base64Prefix.length);
+            //     resolve(base64String.replace(base64Prefix,''));
+            // }
+        };
+        reader.onerror = function (error) {
+          console.log('Error: ', error);
+        };
+    })
+    // var reader = new FileReader();
+    // reader.readAsDataURL(file);
+    // reader.onload = function () {
+    //     var base64String = reader.result;
+    //     const base64Prefix = 'data:image/svg+xml;base64,'
+    //     if(base64String.startsWith(base64Prefix)) {
+    //         base64String = base64String.slice(base64Prefix.length);
+    //         return base64String;
+
+    //     }
+    // };
+    // reader.onerror = function (error) {
+    //   console.log('Error: ', error);
+    // };
  }
